ccc915ff1b43badeafabea17149b5ed36b72a52f
saurabhdhokare001 <165820786+saurabhdhokare001@users.noreply.github.com>
2024-05-04 18:52:21 +0930

    Add files via upload

---
+++ matrix_op.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+
+using namespace std;
+
+class Matrix
+{
+	private:
+
+	int arr[r][c];
+
+	public:
+
+	void input()
+	{
+		int a[n][n];
+		for(int i=0;i<n;i++)
+
+		{	
+			for(int j=0;j<n;j++)
+
+			{
+				cin>>a[i][j];
+			}
+		}
+	}
+	
+	void display()
+	{
+		for(int i=0;i<n;i++)
+		
+		{
+			for(int j=0;j<n;j++)
+			
+			{
+				cout<<a[i][j];
+ 			}
+		}
+	}
+};
+
